5baca2a51c3fbe6ba36a29dabc7c1524ff847557
Rolex96 <110495702+Rolex96@users.noreply.github.com>
2022-08-19 17:27:57 -0300

    Actualización

--- js/login.js
+++ js/login.js
@@ -23,6 +23,7 @@ document.getElementById("buttonx").onclick = function (1) {
 */
 
 
+/*
 const email = document.getElementById("email");
 const contraseña = document.getElementById("contraseña");
 const button = document.getElementById("button-ingresar");
@@ -41,7 +42,25 @@ const seCumple = true;
         window.location.href = "index.html";
         }
       } })
+*/
 
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("button-ingresar").addEventListener("click", function(){
+        let email = document.getElementById("email").value;
+        let contraseña = document.getElementById("contraseña").value;
+        let condicion = true;
 
-   
+        if (email == '') {
+            condicion = false;
+            document.getElementById("alert-email").innerHTML = "Introduzca un Email válido";
+        }
+        if (contraseña == '') {
+            condicion = false;
+            document.getElementById("alert-contr").innerHTML = "Introduzca una contraseña válida";
+        }
+        if (condicion){
+            window.location.href = "portada.html";
+        }
+    })
+})   
       
